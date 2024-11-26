2302dfe5a9a1d1e209581ecb1f6de47bce26caac
Vicheka Oeun <oeunvicheka95@gmail.com>
2024-11-25 19:02:35 -0500

    add auth check on dashboard

--- app/dashboard/page.js
+++ app/dashboard/page.js
@@ -1,7 +1,42 @@
+'use client'
+import { supabase } from '../../lib/client';
+import { useEffect, useState } from 'react';
+import { useRouter } from 'next/navigation';
 export default function Dashboard(){
+    const router = useRouter();
+    const [user, setUser] = useState(null); //hold user object
+    const [loading, setLoading] = useState(true);
+    useEffect(() =>{
+        async function getUser(){
+            const {data : {user}} = await supabase.auth.getUser();
+            if (!user){
+                router.push('/auth');
+            } else{
+                setUser(user);
+            }
+            setLoading(false);
+        }
+        getUser();
+    }, [])
+
+    async function handleSignOut(event){
+        await supabase.auth.signOut();
+        router.push('/auth');
+    }
+    
+    if (loading) return <>Loading...</>
+    if (!user) return null;
+
     return(
-        <>
-        <p>Authentication complete</p>
-        </>
+        <div className="p-4">
+            <h1 className="text-2xl mb-4">Dashboard</h1>
+            <p>Welcome {user.email}</p>
+            <button 
+                onClick={handleSignOut}
+                className="mt-4 px-4 py-2 bg-red-500 text-white rounded"
+            >
+                Sign Out
+            </button>
+      </div>
     )
 }
