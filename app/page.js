074f2062162d21c9ed2b99bf50307463b6ded503
sarahrosehassan <sarahrosehassan@outlook.com>
2024-11-28 13:03:50 -0500

    Made some  minor changes to the page.js homepage for testing.

--- app/page.js
+++ app/page.js
@@ -1,3 +1,12 @@
-export default function App(){
-    return <h1>Main Page</h1>
-}
+export default function App() {
+    return (
+      <div>
+        <h1>Welcome to TMU GradTrack</h1>
+        <p>Easily track and manage graduate progress with TMU GradTrack.</p>
+        <button style={{ padding: "10px 20px", backgroundColor: "#2148C0", color: "#FFF", border: "none", borderRadius: "5px" }}>
+          Get Started
+        </button>
+      </div>
+    );
+  }
+  
